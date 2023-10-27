868e8bc6be348aa9a55772f9cba74fc5bbe0b011
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-10-27 22:30:15 +0530

    encoding message.cpp

---
+++ SecondYear/29deepanshutyagi_DeepanshuTyagi_2226cs1195_2/Week_4/encodingmessage.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+string encode(string &message)
+{
+    //   Write your code here.
+     string encoded_message = "";
+  int count = 1;
+  char current_char = message[0];
+  for (int i = 1; i < message.length(); i++) {
+    if (message[i] == current_char) {
+      count++;
+    } else {
+      encoded_message += current_char + to_string(count);
+      current_char = message[i];
+      count = 1;
+    }
+  }
+  encoded_message += current_char + to_string(count);
+  return encoded_message;
+}
